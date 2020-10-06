25a02cd8a4ed1e723b8e6745d7fca6dbee1820c6
meyash <yashsn2127@gmail.com>
2020-10-06 16:57:05 +0530

    sudoku_s

---
+++ 7_backtracking/3_sudoku.cpp
@@ -0,0 +1,42 @@
+// https://www.codechef.com/problems/AX06
+
+// Example
+// Input:
+// 023456789
+// 406789123
+// 780123456
+// 234067891
+// 567801234
+// 891230567
+// 345678012
+// 678912305
+// 912345670
+
+
+// Output:
+// 123456789
+// 456789123
+// 789123456
+// 234567891
+// 567891234
+// 891234567
+// 345678912
+// 678912345
+// 912345678
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main(){
+    // used for fast IO
+    // ios_base::sync_with_stdio(false);cin.tie(NULL);
+    // sets precision for required datatype
+    // cout.precision(numeric_limits<double>::max_digits10);
+	int t;
+	cin>>t;
+	while(t--){
+        
+	}
+	return 0;
+}
