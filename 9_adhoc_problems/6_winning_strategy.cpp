04b7b74f64ca45748c5e3a384841182e43bad8d0
meyash <yashsn2127@gmail.com>
2020-10-10 20:44:19 +0530

    _adhoc_winning

--- 9_adhoc_problems/6_winning_strategy.cpp
+++ 9_adhoc_problems/6_winning_strategy.cpp
@@ -30,3 +30,24 @@
 //     1 2 3 4 5 -> 1 2 3 5 4 -> 1 2 5 3 4 -> 2 1 5 3 4
 // In the second case, no way to form this specific order
 
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int n;
+    cin>>n;
+    int order[n];
+    for(int i=0;i<n;i++){
+        cin>>order[i];
+    }
+    int diff=0;
+    for(int i=0;i<n;i++){
+        if(abs((i+1)-order[i])>2){
+            cout<<"NO"<<endl;
+            return 0;
+        }
+        diff+=abs((i+1)-order[i]);
+    }
+    cout<<"YES"<<endl;
+    cout<<diff/2<<endl;
+    return 0;
+}
