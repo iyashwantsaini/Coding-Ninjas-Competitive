89bc307abc0a7a68dbddeebd944ba6bef5e72a6f
meyash <yashsn2127@gmail.com>
2020-10-21 21:11:48 +0530

    coin_chnge

--- 11_dynamic_1/6_coin_change.cpp
+++ 11_dynamic_1/6_coin_change.cpp
@@ -24,9 +24,49 @@ Number of ways are - 4 total i.e. (1,1,1,1), (1,1,2), (1,3) and (2,2).
 using namespace std;
 typedef long long ll;
 
+int find_ans(int den[],int start,int size,int curr_val,int goal,int **storage){
+  // base cases
+  // current val greater than goal
+  if(curr_val>goal){
+    return 0;
+  }
+  // goal value reached
+  if(curr_val==goal){
+    return 1;
+  }
+  // checked all denominations
+  if(start==size){
+    return 0;
+  }
+  if(storage[size-start][curr_val]>0){
+    return storage[size-start][curr_val];
+  }
+  // starting element of den included
+  int leftpossible=find_ans(den,start,size,curr_val+den[start],goal,storage);
+  // starting element of den not included
+  int rightpossible=find_ans(den,start+1,size,curr_val,goal,storage);
+  storage[size-start][curr_val]=leftpossible+rightpossible;
+  return leftpossible+rightpossible;
+}
 
 int countWaysToMakeChange(int denominations[], int numDenominations, int value){
-    
+  sort(denominations,denominations+numDenominations);
+  int **storage=new int*[numDenominations+1];
+  for(int i=0;i<numDenominations+1;i++){
+    storage[i]=new int[value+1];
+  }
+  // set all storage elements to -1
+  for(int i=0;i<numDenominations+1;i++){
+    for(int j=0;j<value+1;j++){
+      storage[i][j]=-1;
+    }
+  }
+  int ans=find_ans(denominations,0,numDenominations,0,value,storage);
+  for(int i=0;i<numDenominations+1;i++){
+    delete storage[i];
+  }
+  delete storage;
+  return ans;
 }
 
 int main(){
