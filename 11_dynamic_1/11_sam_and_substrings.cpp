b8132c38226f1f7109e2798015b60ade4147bbd4
meyash <yashsn2127@gmail.com>
2020-10-23 17:55:24 +0530

    sam&substrings

--- 11_dynamic_1/11_sam_and_substrings.cpp
+++ 11_dynamic_1/11_sam_and_substrings.cpp
@@ -1,2 +1,36 @@
 // https://www.hackerrank.com/challenges/sam-and-substrings/problem
 
+/*
+INPUT
+123
+OUTPUT
+164
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Complete the substrings function below.
+int substrings(string n) {
+    int m=1000000007;
+    long long sum=0,last=0;
+    sum=n[0]-'0';
+    last=n[0]-'0';
+    if(n.length()==1){
+        return sum;
+    }
+    for(int i=1;i<n.length();i++){
+        last=(last*10+(n[i]-'0')*(i+1))%m;
+        sum=(sum+last)%m;
+    }
+    return sum;
+}
+
+int main()
+{
+    string n;
+    getline(cin, n);
+    int result = substrings(n);
+    cout << result << "\n";
+    return 0;
+}
