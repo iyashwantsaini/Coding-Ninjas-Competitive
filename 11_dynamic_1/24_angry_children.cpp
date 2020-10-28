e82f02a17ff2008d9e61920129581a16a2dfb27f
meyash <yashsn2127@gmail.com>
2020-10-29 00:32:22 +0530

    _angrychildren

--- 11_dynamic_1/24_angry_children.cpp
+++ 11_dynamic_1/24_angry_children.cpp
@@ -33,3 +33,53 @@ Sample Output
 Explanation
 Bill Gates will choose packets having 10, 20 and 30 candies.So unfairness will be |10-20| + |20-30| + |10-30| = 40. We can verify that it will be minimum in this way.
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+    ll n,k;
+    cin>>n>>k;
+    ll candycount[n+1]{};
+    for(ll i=0;i<n;i++){
+        cin>>candycount[i];
+    }
+
+    // nlogn | sort the array to bring closer values together
+    sort(candycount,candycount+n);
+
+    // dp array to store the unfairness of previous k elements at each index
+    ll cost[n+1]{};
+    
+    // maintain sum of previous k elements
+    ll sum=0;
+    // save first k's unfairness first at kth index 
+    cost[0]=0; // as only 1 element,so unfairness is 0
+    sum+=candycount[0];
+    for(ll i=1;i<k;i++){
+        cost[i]=cost[i-1]+i*candycount[i]-sum;
+        sum+=candycount[i];
+    }
+
+    // save overall minimum unfairness
+    // currently a possible minimum is unfairness in 1st k
+    ll min_unfairness=cost[k-1];
+
+    // find unfairness in all next possible k's sets
+    ll el_todelete=0;
+    for(ll i=k;i<n;i++){
+        sum-=candycount[el_todelete];
+        cost[i]=cost[i-1]-2*(sum)+(k-1)*candycount[el_todelete]+(k-1)*candycount[i];
+        sum+=candycount[i];
+        el_todelete++;
+
+        if(cost[i]<min_unfairness){
+            min_unfairness=cost[i];
+        }
+
+    }
+
+    cout<<min_unfairness<<endl;
+
+}
