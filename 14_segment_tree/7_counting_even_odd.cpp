98889411e033a695a9c4ea63b6fbbd17d9cd9aff
meyash <yashsn2127@gmail.com>
2020-11-17 21:17:08 +0530

    counting_even_odd

--- 14_segment_tree/7_counting_even_odd.cpp
+++ 14_segment_tree/7_counting_even_odd.cpp
@@ -48,16 +48,94 @@ class node{
         int numeven=0;
 };
 
-void build(){
+class lazynode{
+    public:
+        int evenrem=0;
+        int oddrem=0;
+};
 
+void build(int *a,node *tree,int s,int e,int i){
+    // base case
+    if(s==e){
+        if(a[s]%2==0){
+            tree[i].numeven+=1;
+        }else{
+            tree[i].numodd+=1;
+        }
+        return;
+    }
+    // build children
+    int mid=(s+e)/2;
+    build(a,tree,s,mid,i*2);
+    build(a,tree,mid+1,e,i*2+1);
+    // fill current
+    tree[i].numeven=tree[i*2].numeven+tree[i*2+1].numeven;
+    tree[i].numodd=tree[i*2].numodd+tree[i*2+1].numodd;
 }
 
-void update_set(){
-
+void view_tree(int n,node *tree){
+    for(int i=1;i<4*n;i++){
+        cout<<tree[i].numeven<<" "<<tree[i].numodd<<"\n";
+    }
 }
 
-int query(){
-    return 0;
+void update_set(int *a,node *tree,lazynode *lazy,int s,int e,int i,int index,int val){
+    // out of bound
+    if(s>e){
+        return;
+    }
+    // element reached
+    if(s==e){
+        a[index]=val;
+        if(val%2==0){
+            tree[i].numeven=1;
+            tree[i].numodd=0;
+        }else{
+            tree[i].numodd=1;
+            tree[i].numeven=0;
+        }
+        return;
+    }
+    int mid=(s+e)/2;
+    if(index>mid){
+        update_set(a,tree,lazy,mid+1,e,i*2+1,index,val);
+    }else{
+        update_set(a,tree,lazy,s,mid,i*2,index,val);
+    }
+    tree[i].numeven=tree[i*2].numeven+tree[i*2+1].numeven;
+    tree[i].numodd=tree[i*2].numodd+tree[i*2+1].numodd;
+}
+
+int query_even(node *tree,int s,int e,int i,int l, int r){
+    // completely outside
+    if(l>e||r<s){
+        return 0;
+    }
+    // completely inside
+    if(s>=l&&e<=r){
+        return tree[i].numeven;
+    }
+    // partially in, partially out
+    int mid=(s+e)/2;
+    int lans=query_even(tree,s,mid,i*2,l,r);
+    int rans=query_even(tree,mid+1,e,i*2+1,l,r);
+    return lans+rans;
+}
+
+int query_odd(node *tree,int s,int e,int i,int l, int r){
+    // completely outside
+    if(l>e||r<s){
+        return 0;
+    }
+    // completely inside
+    if(s>=l&&e<=r){
+        return tree[i].numodd;
+    }
+    // partially in, partially out
+    int mid=(s+e)/2;
+    int lans=query_odd(tree,s,mid,i*2,l,r);
+    int rans=query_odd(tree,mid+1,e,i*2+1,l,r);
+    return lans+rans;
 }
 
 int main(){
@@ -67,19 +145,26 @@ int main(){
     for(int i=1;i<=n;i++){
         cin>>a[i];
     }
+    
     node *tree=new node[4*n];
-    build();
+    lazynode *lazy=new lazynode[4*n];
+
+    build(a,tree,1,n,1);
+    // view_tree(n,tree);
+
     int q;
     cin>>q;
     while(q--){
         int qtype,x,y;
         cin>>qtype>>x>>y;
         if(qtype==0){
-            update_set();
+            update_set(a,tree,lazy,1,n,1,x,y);
         }else if(qtype==1){
-            int ans=query();
+            int ans=query_even(tree,1,n,1,x,y);
+            cout<<ans<<endl;
         }else{
-            int ans=query();
+            int ans=query_odd(tree,1,n,1,x,y);
+            cout<<ans<<endl;
         }
     }
     return 0;
