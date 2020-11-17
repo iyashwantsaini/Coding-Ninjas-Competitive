08f2bc7098b215beb0a2c194d4e3c772b09676d6
meyash <yashsn2127@gmail.com>
2020-11-17 22:28:51 +0530

    vasya_rhezo

--- 14_segment_tree/8_vasya_rhezo.cpp
+++ 14_segment_tree/8_vasya_rhezo.cpp
@@ -33,7 +33,139 @@ Sample Output
 using namespace std;
 typedef long long ll;
 
-int main(){
+class warrior{
+    public:
+        int a;
+        int b;
+};
+
+class node{
+    public:
+        int a;
+        int b;
+        int index;
+};
+
+void build_tree(warrior *w,node *tree,int s,int e,int i){
+    // base case
+    if(s==e){
+        tree[i].a=w[s].a;
+        tree[i].b=w[s].b;
+        tree[i].index=s;
+        return;
+    }
+    // build children
+    int mid=(s+e)/2;
+    build_tree(w,tree,s,mid,i*2);
+    build_tree(w,tree,mid+1,e,i*2+1);
+
+    // fix current
+    // set current as the one having greater "A"
+    // if "A" is same set to the one having less "B"
+    // if "A" is same & "B" is same, set to the one having less index
     
+    node left=tree[i*2];
+    node right=tree[i*2+1];
+
+    // choose on basis of a
+    if(left.a>right.a){
+        tree[i].index=left.index;
+        tree[i].a=left.a;
+        tree[i].b=left.b;
+    }else if(right.a>left.a){
+        tree[i].index=right.index;
+        tree[i].a=right.a;
+        tree[i].b=right.b;
+    }else{
+        // choose on basis of b
+        if(left.b<left.b){
+            tree[i].index=left.index;
+            tree[i].a=left.a;
+            tree[i].b=left.b;
+        }else if(right.b<left.b){
+            tree[i].index=right.index;
+            tree[i].a=right.a;
+            tree[i].b=right.b;
+        }else{
+            // choose on basis of index
+            if(left.index<right.index){
+                tree[i].index=left.index;
+                tree[i].a=left.a;
+                tree[i].b=left.b;
+            }else{
+                tree[i].index=right.index;
+                tree[i].a=right.a;
+                tree[i].b=right.b;
+            }
+        }
+    }
+}
+
+void view_tree(int n,node *tree){
+    for(int i=1;i<4*n;i++){
+        cout<<tree[i].index<<" ";
+    }
+    cout<<"\n";
+}
+
+node find_warr(node *tree,int s,int e,int i,int l,int r){
+    // 3 cases
+    // completely out
+    if(l>e||r<s){
+        node ans;
+        ans.a=INT_MIN;
+        ans.b=INT_MAX;
+        ans.index=INT_MAX;
+        return ans;
+    }
+    // completely in
+    if(s>=l&&e<=r){
+        return tree[i];
+    }
+    // partially in, partially out
+    int mid=(s+e)/2;
+    node left=find_warr(tree,s,mid,i*2,l,r);
+    node right=find_warr(tree,mid+1,e,i*2+1,l,r);
+    if(left.a>right.a){
+        return left;
+    }else if(right.a>left.a){
+        return right;
+    }else{
+        if(left.b<right.b){
+            return left;
+        }else if(right.b<left.b){
+            return right;
+        }else{
+            if(left.index<right.index){
+                return left;
+            }else{
+                return right;
+            }
+        }
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    warrior *w=new warrior[n+1];
+    for(int i=1;i<=n;i++){
+        cin>>w[i].a;
+    }
+    for(int i=1;i<=n;i++){
+        cin>>w[i].b;
+    }
+    node *tree=new node[4*n]{};
+    build_tree(w,tree,1,n,1);
+    // view_tree(n,tree);
+
+    int q;
+    cin>>q;
+    while(q--){
+        int l,r;
+        cin>>l>>r;
+        node ans=find_warr(tree,1,n,1,l,r);
+        cout<<ans.index<<endl; 
+    }
     return 0;
 }
