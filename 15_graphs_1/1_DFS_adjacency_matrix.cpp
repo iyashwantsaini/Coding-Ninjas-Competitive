775df86a6c06da343b5f97ab08c0044e62aff717
meyash <yashsn2127@gmail.com>
2020-11-19 16:09:34 +0530

    bfs_dfs_disconnected

--- 15_graphs_1/1_DFS_adjacency_matrix.cpp
+++ 15_graphs_1/1_DFS_adjacency_matrix.cpp
@@ -1,4 +1,24 @@
 /*
+
+Given an undirected and disconnected graph G(V, E), print its DFS traversal.
+Note:
+1. Here you need to consider that you need to print DFS path starting from vertex 0 only. 
+2. V is the number of vertices present in graph G and vertices are numbered from 0 to V-1. 
+3. E is the number of edges present in graph G.
+4. Take graph input in the adjacency matrix.
+5. Handle for Disconnected Graphs as well
+Input Format :
+The first line of input contains two integers, that denote the value of V and E.
+Each of the following E lines contains space separated two integers, that denote that there exists an edge between vertex a and b.
+Output Format :
+Print the DFS Traversal, as described in the task.
+Constraints :
+0 <= V <= 1000
+0 <= E <= (V * (V - 1)) / 2
+0 <= a <= V - 1
+0 <= b <= V - 1
+Time Limit: 1 second
+
 input
 7
 8
@@ -68,7 +88,14 @@ int main(){
     for(int i=0;i<=n;i++){
         visited[i]=0;
     }
-    dfs_print(aj,n,0,visited);
+
+    // as we can have disconnected graphs as well
+    // so we'll need to run a loop to tak ein consideration all the components
+    for(int i=0;i<n;i++){
+        if(visited[i]==0){
+            dfs_print(aj,n,i,visited);
+        }
+    }
     
     
     // delete all memory
