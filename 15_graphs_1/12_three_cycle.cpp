3f49e51d8ff35a5575d01c97c08172a592d0bdc1
meyash <yashsn2127@gmail.com>
2020-11-20 13:23:49 +0530

    added_graph_questions

---
+++ 15_graphs_1/12_three_cycle.cpp
@@ -0,0 +1,28 @@
+/*
+Given a graph with N vertices (numbered from 0 to N-1) and M undirected edges, then count the distinct 3-cycles in the graph. A 3-cycle PQR is a cycle in which (P,Q), (Q,R) and (R,P) are connected by an edge.
+Input Format :
+The first line of input contains two space separated integers, that denotes the value of N and M.
+Each of the following M lines contain two integers, that denote the vertices which have an undirected edge between them. Let us denote the two vertices with the symbol u and v. 
+Output Format :
+Print the count the number of 3-cycles in the given graph
+Constraints :
+0 <= N <= 100
+0 <= M <= (N*(N-1))/2
+0 <= u <= N - 1
+0 <= v <= N - 1
+Time Limit: 1 sec
+Sample Input 1:
+3 3
+0 1
+1 2
+2 0
+Sample Output 1:
+1
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write your code here
+}
