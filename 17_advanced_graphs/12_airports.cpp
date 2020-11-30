c06f73bbd18fdbf9741436f1c41e680c72bb37b1
meyash <yashsn2127@gmail.com>
2020-11-30 21:09:08 +0530

    airports

--- 17_advanced_graphs/12_airports.cpp
+++ 17_advanced_graphs/12_airports.cpp
@@ -27,23 +27,82 @@ Case #2: 2090 2
 #include<bits/stdc++.h>
 using namespace std;
 
+class edge{
+    public:
+        int ei,ej,cost;
+};
+
+int find_parent(int num,int *parents,int *rank){
+    if(num==parents[num]){
+        return num;
+    }
+    int p=find_parent(parents[num],parents,rank);
+    if(rank[p]<=rank[num]){
+        parents[p]=num;
+        rank[num]++;
+    }else{
+        parents[num]=p;
+        rank[p]++;
+    }
+    return p;
+}
+
+bool compare(edge e1,edge e2){
+    return e1.cost<e2.cost;
+}
+
 int main(){
     int t;
     cin>>t;
-    while(t--){
+    for(int iter=1;iter<=t;iter++){
         int n,m,a;
-        int **mat=new int*[n+1];
-        for(int i=0;i<=n;i++){
-            mat[i]=new int[n+1]{};
-        }
         cin>>n>>m>>a;
+        edge *edges=new edge[m+1];
         for(int i=0;i<m;i++){
-            int x,y,c;
-            cin>>x>>y>>c;
-            mat[x][y]=c;
-            mat[y][x]=c;
+            cin>>edges[i].ei>>edges[i].ej>>edges[i].cost;
         }
-        
+        sort(edges,edges+m,compare);
+        // initially all will be parents of itself
+        int *parents=new int[n+1];
+        for(int i=0;i<=n;i++){
+            parents[i]=i;
+        }
+        // set rank for each vertex
+        int *rank=new int[n+1]{};
+        // connect those vertices with roads which have less cost
+        // than building an airport
+        int cost=0;
+        int j=0;
+        for(int i=0;i<m&&j<n-1;i++){
+            edge e=edges[i];
+            // build a road if cost of building a road is less than airport
+            // as we have already sorted we dont need to do this
+            if(e.cost>=a){
+                break;
+            }
+            // find parents of both vertices
+            int p1=find_parent(e.ei,parents,rank);
+            int p2=find_parent(e.ej,parents,rank);
+            if(p1!=p2){
+                cost+=e.cost;
+                if(rank[p1]<=rank[p2]){
+                    parents[p1]=p2;
+                    rank[p2]++;
+                }else{
+                    parents[p2]=p1;
+                    rank[p1]++;
+                }
+                j++;
+            }
+        }
+        int numair=0;
+        for(int i=1;i<=n;i++){
+            if(i==parents[i]){
+                cost+=a;
+                numair+=1;
+            }
+        }
+        cout<<"Case #"<<iter<<": "<<cost<<" "<<numair<<"\n";
     }
     return 0;
 }
