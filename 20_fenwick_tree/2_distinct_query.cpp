33563b0f58bff2929506c7e8ae16b338a21e1a64
meyash <yashsn2127@gmail.com>
2020-12-12 15:59:18 +0530

    distinct_query

--- 20_fenwick_tree/2_distinct_query.cpp
+++ 20_fenwick_tree/2_distinct_query.cpp
@@ -23,6 +23,96 @@ Sample Output
 #include <bits/stdc++.h>
 using namespace std;
 
+class node{
+    public:
+        int s,e,index;
+};
+
+bool compare(node a,node b){
+    if(a.e==b.e){
+        return a.s<b.s;
+    }
+    return a.e<b.e;
+}
+
+void update(int n,int index,int *BIT,int type){
+    // type 0=>increment
+    if(type==0){
+        for(;index<=n;index+=index&(-index)){
+            BIT[index]++;
+        }
+    }
+    // type 1=>decrement
+    else{
+        for(;index<=n;index+=index&(-index)){
+            BIT[index]--;
+        }
+    }
+}
+
+int query(int index,int *BIT){
+    int count=0;
+    for(;index>0;index-=index&(-index)){
+        count+=BIT[index];
+    }
+    return count;
+}
+
 int main(){
+    int n;
+    cin>>n;
+    int *a=new int[n+1];
+    for(int i=1;i<=n;i++){
+        cin>>a[i];
+    }
+    int q;
+    cin>>q;
+    node *queries=new node[q];
+    for(int i=0;i<q;i++){
+        cin>>queries[i].s>>queries[i].e;
+        queries[i].index=i;
+    }
+    // sort on basis of end index
+    sort(queries,queries+q,compare);
+
+    int *BIT=new int[n+1]{};
+    // k=>maintains index in queries
+    int k=0;
+    // prev=>to maintain last occurance index of all elements
+    int *prev=new int[1000001];
+    for(int i=0;i<=1000000;i++){
+        prev[i]=-1;
+    }
+    // total=> to maintain the number of elements till current element
+    int total=0;
+    // ans=>to store answers of queries in correct incoming order
+    int *ans=new int[q+1]{};
+    for(int i=1;i<=n;i++){
+        // if previous of current does not exist => means its unique => update
+        if(prev[a[i]]==-1){
+            // increment in BIT
+            update(n,i,BIT,0);
+            // increment total number of elements
+            total++;
+        }
+        // remove previous existance of element, add current
+        else{
+            // decrement previous occurance of current by 1
+            update(n,prev[a[i]],BIT,1);
+            // increment in BIT
+            update(n,i,BIT,0);
+        }
+        // now we can get answers for querues ending at this ith index
+        while(k<q&&queries[k].e==i){
+            // ans[queries[k].index]=query(queries[k].e,BIT)-query(queries[k].s-1,BIT);
+            // OR
+            ans[queries[k].index]=total-query(queries[k].s-1,BIT);
+            k++;
+        }
+        prev[a[i]]=i;
+    }
+    for(int i=0;i<q;i++){
+        cout<<ans[i]<<"\n";
+    }
     return 0;
 }
