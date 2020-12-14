733d92227542077650e70235d48dabc179307e56
meyash <yashsn2127@gmail.com>
2020-12-14 12:58:57 +0530

    shilwave

--- 20_fenwick_tree/5_shil_wave.cpp
+++ 20_fenwick_tree/5_shil_wave.cpp
@@ -20,8 +20,66 @@ All the possible sequences are: [ 1 3 ] , [1 5 ] , [ 1 4 ] , [1 2 ] , [1 3 2] , 
 
 #include<bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+#define mod 1000000007
+
+void update(int index,int **BIT,int type,int val,int n){
+	for(;index<=100001;index+=index&(-index)){
+		BIT[index][type]=(BIT[index][type]+val)%mod;
+	}
+}
+
+int query(int index,int **BIT,int type,int n){
+	int count=0;
+	for(;index>0;index-=index&(-index)){
+		count=(count+BIT[index][type])%mod;
+	}
+	return count;
+}
 
 int main(){
+	int n;
+	cin>>n;
+	int *a=new int[n+1]{};
+	int maxtill=INT_MIN;
+	for(int i=1;i<=n;i++){
+		cin>>a[i];
+		maxtill=max(maxtill,a[i]);
+	}
+	// 3 binary index trees
+	// 0 => for all subseq j<i || a[j]<a[i]
+	// 1 => for all subseq j<i || a[j]>a[i] 
+	// 2 => for how many a[j]'s are smaller than a[i]
 
+	// creating 3 BIT's
+	int **BIT=new int*[100001];
+	for(int i=0;i<=100000;i++){
+		BIT[i]=new int[3]{};
+	}
+	ll ans=0;
+	for(int i=1;i<=n;i++){
+		int el=a[i];
+
+		// for values less than a[i]
+		// 0=> find sigma(a[j]<a[i]) part by finding for a[i]-1
+		//  2=> for sigma(1) part
+        int a=(query(el-1,BIT,0,n)+query(el-1,BIT,2,n))%mod;
+
+		// now for values grater than a[i]
+		int b=(query(maxtill,BIT,1,n)-query(el,BIT,1,n)+query(maxtill,BIT,2,n)-query(el,BIT,2,n))%mod;
+
+		// updating calculated values in all BIT's
+		update(el,BIT,0,b,n);
+		update(el,BIT,1,a,n);
+		update(el,BIT,2,1,n);
+
+        // saving to total ans
+        // ans=a+b;
+		a=(a+mod)%mod;
+        b=(b+mod)%mod;
+		ans=(ans+a)%mod;
+        ans=(ans+b)%mod;
+	}
+	cout<<ans<<"\n";
 	return 0;
 }
